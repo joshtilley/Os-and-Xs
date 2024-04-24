57be0e16dd3aeaa8488415f0add35719c09add13
joshtilley <164543126+joshtilley@users.noreply.github.com>
2024-04-24 19:56:14 +0100

    added ui controller

--- app.js
+++ app.js
@@ -62,16 +62,27 @@ const Gameplay = function () {
     // defines players and assigns tokens
     const players = [
         {
-            name: "player1",
+            name: "Player One",
             token: 1,
             display: `<i class="fa-regular fa-circle"></i>`
         },
         {
-            name: "player2",
+            name: "Player Two",
             token: 2,
             display: `<i class="fa-regular fa-x"></i>`
         }
     ]
+    
+    // sets player names
+    const submit_btn = document.getElementById('submit');
+    submit_btn.addEventListener('click', () => {
+        players[0].name = document.getElementById('player1-name').value;
+        players[1].name = document.getElementById('player2-name').value;
+        
+        document.querySelector('.names-modal').classList.add('hide');
+        document.querySelector('.modal-container').classList.add('hide');
+        document.querySelector('#new-game').classList.remove('hide');
+    })
     
     // sets the player to take the first turn
     let activePlayer = players[0];
@@ -102,10 +113,10 @@ const Gameplay = function () {
 
 // function will check for legal moves. a legal move requires a free cell, and that there has been no winner. 
 const checkLegalMove = (board) => {
-    if (board.map(row => row.filter(col => col === 0).join("")).join("").length === 0) {return 1}
-
-    let gamewinner = checkForWinner(board);
-    if (gamewinner.indexOf(1) !== -1) {return 2}
+    // if checkForWinner returns an array containing 1, game is won
+    if (checkForWinner(board).indexOf(1) !== -1) return 2;
+    // if board contains no zero values, game is a draw
+    if (board.map(row => row.filter(col => col === 0).join("")).join("").length === 0) return 1;
 
     else return 0;
 } 
@@ -152,25 +163,58 @@ const checkForWinner = (board) => {
 
 // beginning of UI script
 const GameController = function() {
+
     // initialises the game
     let game = Gameplay();
 
     const gameboard_div = document.querySelector('.gameboard');
+
+    // toggles display of the round's result on game end
+    const matchResult_modal = document.querySelector('.results-modal');
+    matchResult_modal.addEventListener('click', () => {
+        matchResult_modal.classList.add('hide');
+        document.querySelector('.modal-container').classList.add('hide')
+    });
     
+    function displayMatchResult(outcome) {
+        if (outcome === 1) {
+            matchResult_modal.innerHTML = `<div class="result">It's a draw!</div>`
+        }
+        if (outcome === 2) {
+            matchResult_modal.innerHTML = `<div class="result">${game.getActivePlayer().name} wins!</div>`
+        }
+        document.querySelector('.modal-container').classList.remove('hide')
+        matchResult_modal.classList.remove('hide')
+    }
+    
+    // removes pointer cursor from all cells when game is finished
+    function deactivateCells() {
+        for(let row = 0; row < 3; row++){
+            for (let cell = 0; cell < 3; cell++){
+                let cell_Id = '' + row + cell;
+                document.getElementById(cell_Id).classList.remove('cell-active');
+            };
+        };
+    }
+
+    // main gameplay loop
     function onClickHandler(e) {
         if (!e.target.id) return;
         let targetedCell = document.getElementById(`${e.target.id}`);
         targetedCell.innerHTML = `${game.getActivePlayer().display}`;
+        targetedCell.classList.remove('cell-active')
         game.playTurn(e.target.id);
-
+        // logic to check for winner or tied game
         switch (checkLegalMove(game.board())) {
             case 1:
-                console.log("No legal moves remain... Draw.");
                 gameboard_div.removeEventListener('click', onClickHandler);
+                deactivateCells();
+                displayMatchResult(1);
                 break;
             case 2:
-                console.log(`${game.getActivePlayer().name} wins!`);
                 gameboard_div.removeEventListener('click', onClickHandler);
+                deactivateCells();
+                displayMatchResult(2);
                 break;
             case 0:
                 game.switchPlayer();
@@ -185,7 +229,8 @@ const GameController = function() {
         for(let row = 0; row < 3; row++){
             for (let col = 0; col < 3; col++) {
                 let cell_Id = "" + row + col
-                document.getElementById(`${cell_Id}`).innerHTML = ""
+                document.getElementById(`${cell_Id}`).innerHTML = "";
+                document.getElementById(`${cell_Id}`).classList.add('cell-active');
             }
         }
         // initialises backend game state
@@ -198,5 +243,15 @@ const GameController = function() {
     newGame_btn.addEventListener('click', restartGame)
 }
 
-GameController()    
+GameController()
 
+// general UI functionality
+const playerName_inputs = document.querySelectorAll('.name-input')
+playerName_inputs.forEach(
+    (input) => input.addEventListener('click', () => input.select())
+);
+
+window.addEventListener('DOMContentLoaded', () => {
+    playerName_inputs.forEach(
+        (input) => input.value = input.defaultValue)
+})
